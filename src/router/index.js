087c782c82a641bc18d9c3a23fe1ae7e677b59ae
tonyamaksimets <berg_antonina@mail.ru>
2025-02-19 18:47:49 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,9 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
 
+console.log(routes);
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 })
 
